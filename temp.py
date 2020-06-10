870a3f6dec5cbcee43986591fe2c301b5bae78a0
SoramKim <50056220+SoramKim@users.noreply.github.com>
2020-06-10 23:32:38 +0900

    Update temp.py

--- temp.py
+++ temp.py
@@ -5,14 +5,15 @@ import RPi.GPIO as GPIO
 import Adafruit_DHT as dht
 
 sensor = dht.DHT11
-temp_pin = 4
-fan_pin = 21
-
+temp_pin = 7
+fan_pin = 40
+red= 17
+green= 27
 
 def setupFan():
-    GPIO.setmode(GPIO.BCM)
     GPIO.setup(fan_pin, GPIO.OUT)
     GPIO.setwarnings(False)
+
 
 def printTemp():
     h, t = dht.read_retry(sensor, temp_pin)
@@ -26,7 +27,8 @@ def printTemp():
     # else:
     #     GPIO.output(fan_pin, False)
 
-# 
+
+#
 # def checkTemperature():
 #     if t> maxTmp:
 #         GPIO.output(fan_pin, True)
@@ -35,22 +37,25 @@ def printTemp():
 
 
 try:
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(red,GPIO.OUT,initial=GPIO.LOW)
+    GPIO.setup(green, GPIO.OUT, initial=GPIO.LOW)
     setupFan()
-    
+
     while True:
         printTemp()
-        GPIO.output(fan_pin, True)
+        GPIO.output(fan_pin,GPIO.HIGH)
+        GPIO.output(red,GPIO.HIGH)
+        GPIO.output(green, GPIO.HIGH)
         print("fan on")
         time.sleep(5)
-        GPIO.output(fan_pin, False)
+        
+        GPIO.output(fan_pin,GPIO.LOW)
+        GPIO.output(red, GPIO.LOW)
+        GPIO.output(green, GPIO.HIGH)
         print("fan off")
         time.sleep(5)
-        GPIO.output(fan_pin, True)
-        print("fan on")
-        time.sleep(5)
-        GPIO.output(fan_pin, False)
-        print("fan off")
-        time.sleep(5)
+
 
 except KeyboardInterrupt:
     print("Terminated by keyboard")
