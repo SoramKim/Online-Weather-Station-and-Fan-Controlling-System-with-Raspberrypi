e66c7cca3d52e38c86a87be631c1ba38f1fd6ab2
SoramKim <Soram523@gmail.com>
2020-06-11 16:49:08 +0100

    modify php file

--- printTemp.php
+++ printTemp.php
@@ -1,6 +1,14 @@
+<html>
+<body>
+
+<h1>Welcome to Soram's Temperature Station </h1> 
 <?php 
 $re=exec("cat /home/pi/final/OSS-Final-Project/tempLog"); 
-$tem_c=explode(" ",$re); //현재 시간 과 온도값을 천으로 나누고, 소숫점 둘째자리에서 반올림 해서 출력한다. 
-echo ( "Time = ".date("Y-m-d H:i:s")." , Temperature = ".$tem_c[1]." C "); 
+$tem_c=explode(" ",$re); 
+echo ( "Time = ".date("Y-m-d H:i:s")." , Temperature = ".$tem_c[0]." C "." , Humidity = ".$tem_c[1]); 
 ?>
+
+</body>
+</html>
+
 
